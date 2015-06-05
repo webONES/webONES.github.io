b1b9a4908493e41874b1db83129ca5e3d5e62d55
Arandi Lopez <arandilopez.93@gmail.com>
2015-06-04 23:11:39 -0500

    new rake task to make pages, page layout, navbar links, footer and navbar includes, and manifesto

--- Rakefile.rb
+++ Rakefile.rb
@@ -6,7 +6,7 @@ require "stringex"
 
 posts_dir       = "_posts"    # directory for blog files
 new_post_ext    = "md"  # default new post file extension when using the new_post task
-
+new_page_ext    = "md"
 
 #############################
 # Create a new Post #
@@ -33,6 +33,35 @@ task :new_post, :title do |t, args|
     post.puts "author: #{author}"
     post.puts "modified: #{Time.now.strftime('%Y-%m-%d %H:%M:%S %z')}"
     post.puts "---"
+  end
+end
+
+#############################
+# Create a new Page         #
+#############################
+
+# usage rake new_page
+desc "Create a new page"
+task :new_page, :title do |t, args|
+  if args.title
+    title = args.title
+  else
+    title = get_stdin("Enter a title for your page: ")
+  end
+  filename = "#{title.to_url}.#{new_page_ext}"
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  tags = get_stdin("Enter tags to classify your page (comma separated): ")
+  puts "Creating new page: #{filename}"
+  open(filename, 'w') do |page|
+    page.puts "---"
+    page.puts "layout: page"
+    page.puts "permalink: /#{title.to_url}/"
+    page.puts "title: \"#{title}\""
+    page.puts "modified: #{Time.now.strftime('%Y-%m-%d %H:%M')}"
+    page.puts "tags: [#{tags}]"
+    page.puts "---"
   end
 end
 
---
+++ software-engineering-manifesto.md
@@ -0,0 +1,10 @@
+---
+layout: page
+permalink: /software-engineering-manifesto/
+title: "Software Engineering Manifesto"
+modified: 2015-06-04 22:44
+tags: [software, sw, manifesto]
+---
+### The Software Engineering Manifesto by webONES FMAT
+
+> Un Ingeniero de Software nunca abrirá Dreamweaver.
